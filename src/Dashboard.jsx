ce2e0e23fddd96d5d94a61df701c145ced5c32c0
Sameer Palkar <palkarsameer04@gmail.com>
2024-04-21 23:31:51 +0530

    Second Commit

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,5 +1,17 @@
 import Navbar from "./Components/Navbar"
+import './App.css'
 
 export default function Dashboard() {
-    return <div><Navbar></Navbar></div>
+    return <div><Navbar/>
+    <main className="container">
+        <div className="text">
+            <h1>SAY YES</h1>
+            <h3>TO YOUR FAVORITE MEALS</h3>
+        </div>
+        <div className="image">
+            <img src="/3215.jpg"></img>
+        </div>
+    </main>
+    </div>
+
 }
